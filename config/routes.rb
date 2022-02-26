2f88125f563fe457fe2571517f63df49a89766a7
Marcelo Fernandes de Araújo <marcelomaidden@gmail.com>
2022-02-26 17:07:37 -0300

    Adds completed and uncompleted routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   resources :users
-  resources :tasks
+  resources :tasks do
+    collection do
+      get :completed
+      get :uncompleted
+    end
+  end
 
   post 'login', to: "users#login"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
