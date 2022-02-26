2553437be40473cb49f5503299323c3099910141
Marcelo Fernandes de Araújo <marcelomaidden@gmail.com>
2022-02-26 16:02:29 -0300

    Handles task creation

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,26 @@
 class TasksController < ApplicationController
-
   def index
     @tasks = Task.from_user(current_user.id)
 
     render json: { tasks: @tasks }
+  end
+
+  def create
+    @task = Task.new(task_params.merge({ user_id: current_user.id }))
+
+    if @task.save
+      render json: { status: "success", task: @task }
+    else
+      render json: { status: "error", errors: @task.errors }
+    end
+
+  rescue ArgumentError => e
+    render json: { status: "error", errors: e }
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :status)
   end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,6 @@
 class Task < ApplicationRecord
+  STATUS = { uncompleted: false, completed: true }
+  enum status: STATUS
   validates :title, presence: true, length: { in: 6..50 }
-
   scope :from_user, ->(user_id) { where(user_id: user_id) }
 end
