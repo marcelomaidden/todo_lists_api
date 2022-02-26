3b4a7f9aef72be3163040b16865b7661d18f60f5
Marcelo Fernandes de Araújo <marcelomaidden@gmail.com>
2022-02-26 13:49:58 -0300

    Handles token authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,26 @@
 class ApplicationController < ActionController::API
+  before_action :token_based_auth
+
+  def token_based_auth
+    unless current_user.present?
+      return render json: { status: "error", message: "Authentication required" }
+    end
+
+    current_user
+  end
+
+  def current_user
+    return false unless token
+    OpenStruct.new(jwt_decoded_token[0]["user"])
+  end
+
+  private
+
+  def token
+    request.headers['Authorization'].split.last
+  end
+
+  def jwt_decoded_token
+    JwtAuthService.decode_token(token)
+  end
 end
